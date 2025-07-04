dd34e6a88b319ee5d646f5453cf06cb7a887d8d0
Daniel Gomez <dgm1979@gmail.com>
2025-07-04 15:29:53 -0400

    Actualización de programa y readme, se argrega requirements

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Este es un proyecto personal par verificar imagnes ISO mediante la función SHA256.
 
+Requiere librerías del archivo "requirements.txt" para funcionar.  Pueden instalar de la siguiente manera:
+
+pip install -r requirements.txt
+
 Solo deben ejecutar el archivo y agragar el codigo SHA256sum que le entregue el 
 
 python sha256sum.py [codigo SHA256sum]
--- sha256sum.py
+++ sha256sum.py
@@ -6,37 +6,47 @@ import sys
 class sha256sum():
     
     def __init__(self):
+        # Inicia objeto con tres atributos de texto sum, path del archivo y objeto sha256_hash
         self.__sum, self.__file, self.__sha256_hash = None, None, None
-        if len(sys.argv)==2: 
+        if len(sys.argv)==2:
+            # Si hay argumnto en script lo va a ingresar en la variable sum
             self.__sum = sys.argv[1]
+            # Abre dialogo para otener path del archivo
             self.__file = easygui.fileopenbox()
+            # Crea objeto hashlib
             self.__sha256_hash = hashlib.sha256()
             
-    def __get_sum(self):
+    def __get_sum(self) -> bool:
         if self.__file and self.__sum:
-            print('*** SHA256sum ***\n')
+            print('*** SHA256sum ***')
+            # Si existe archivo y texto sum, recorreá el archivo.
             with open(self.__file, "rb") as f:
+                print('Analizando archvio', self.__file.split('/')[-1])
+                # Obtendrá el numero de partes de lectura
                 n = 0
                 while True:
                     if not f.read(4096):
                         break
                     n+=1
-                f.close()    
-
+                f.close()
+                
+            # Vuelve a procesar archivo esta vez ingresando partes a ojeto hashlib
             with open(self.__file, "rb") as f:
-                for i in tqdm(range(n)):
+                for i in tqdm(range(n), desc='Procesando Archivo'):
                     self.__sha256_hash.update(f.read(4096))
                 f.close()
+            # Devuelve booleano si realiza o no el proceso
             return True
         return False
 
     def check_sum(self):
+        # Si realiza el checkeo entrega informe de lo realizado
         if self.__get_sum():
-            print('Archivo : ', self.__file.split('/')[-1])
-            print('SHA256sum : ', self.__sum)
-            print('SHA256sum archivo: ', self.__sha256_hash.hexdigest())
-            print('Estado : ', self.__sha256_hash.hexdigest()==self.__sum) 
+            print('\n*** Resultado ***')
+            print('SHA256sum referancia :', self.__sum)
+            print('SHA256sum archivo    :', self.__sha256_hash.hexdigest())
+            print('Estado               :', self.__sha256_hash.hexdigest()==self.__sum) 
         else:
-            print('Operación Cancelada')
+            print('\n*** Operación Cancelada ***')
 
 sha256sum().check_sum()
